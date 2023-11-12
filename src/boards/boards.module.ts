15a1b6fc6eaf5b3058230ffb79c7d00a1f5207dc
LeeInBok <bbock1214@gmail.com>
2023-11-13 00:39:50 +0900

    sectiond3 CRUD & DTO lecture end

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
-@Controller('/boards')
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService]
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards: Board[] = [];
+
+    getAllboards(): Board[]{
+        return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto){
+        const { title, description } = createBoardDto;
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
+    }
+
+    getBoardById(id: string): Board{
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: string): void{
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus): Board{
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+    }
+}
