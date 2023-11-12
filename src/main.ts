8a7af39e409a8023876e3fe3c77ab3a105e5ff77
LeeInBok <bbock1214@gmail.com>
2023-11-13 03:43:53 +0900

    section4 Excetption Handling & use Pipe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Global Level의 Pipe 설정 (Built in Pipe)
+  // ValidationPipe
+  // ParseIntPipe 
+  // ParseBoolPipe
+  // ParseArrayPipe
+  // ParseUUIDPipe
+  // DefaultValuePipe
+  // app.useGlobalPipes(GlobalPipes);
   await app.listen(3000);
 }
 bootstrap();
