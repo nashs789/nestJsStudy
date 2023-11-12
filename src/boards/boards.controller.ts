b928269346b714cbe669aab8822f07d38e935bc4
LeeInBok <bbock1214@gmail.com>
2023-11-13 00:51:50 +0900

    rollback to previous branch 'section3'

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,8 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './boards.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
     // 생성자 안에서 접근 제한자와 같이 쓰이면 class의 property로 등록된다.
     constructor(private boardService: BoardsService) {}
+
+    @Get('/')
+    getAllBoard(): Board[]{
+        return this.boardService.getAllboards();
+    }
+
+    @Post('/')
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+        return this.boardService.createBoard(createBoardDto);
+    }
+
+    // @Param() params: string[]
+    @Get('/:id')
+    getBoardById(@Param('id')id: string): Board {
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id')id: string): void {
+        this.boardService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string, 
+        @Body('status') status: BoardStatus
+        ): Board{
+        return this.updateBoardStatus(id, status);
+    }
 }
